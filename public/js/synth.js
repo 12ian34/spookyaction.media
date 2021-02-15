ca63f05a6a249cacee35bc096ea9599822e57f2b
Ian Ahuja <12ian34@gmail.com>
2021-02-15 15:47:17 +0000

    whle at home

--- public/js/synth.js
+++ public/js/synth.js
@@ -2,17 +2,40 @@
 // console.log(inputDistortion)
 // console.log(inputDistortion.value)
 
-// Get the text field that we're going to track
-let inputDistortion = document.getElementById("inputDistortion");
-let inputPingPong = document.getElementById("inputPingPong");
-let inputReverb = document.getElementById("inputReverb");
+let inputDistortion = document.getElementById('inputDistortion');
+let inputPingPongDelay = document.getElementById('inputPingPongDelay');
+let inputPingPongFeedback = document.getElementById('inputPingPongFeedback');
+let inputReverb = document.getElementById('inputReverb');
+
+// var inputDataSynth = {
+//     'inputDistortion': 0,
+//     'inputPingPong': 0,
+//     'inputReverb': 0,
+// }
+// localStorage.setItem('dataKey', JSON.stringify(inputDataSynth));
+
+// var val = localStorage.getItem('dataKey');
+
+// if (localStorage.getItem(inputDataSynth)) {
+//     inputDistortion.value = JSON.parse(localStorage.getItem("inputDistortion"));
+//     inputPingPong.value = JSON.parse(localStorage.getItem("inputPingPong"));
+//     inputReverb.value = JSON.parse(localStorage.getItem("inputReverb"));
+// } else {
+//     inputDistortion.value = JSON.parse(inputDataSynth("inputDistortion");
+//     inputPingPong.value = JSON.parse(inputDataSynth("inputPingPong");
+//     inputReverb.value = JSON.parse(inputDataSynth("inputReverb");
+// }
 
 if (localStorage.getItem("inputDistortion")) {
     inputDistortion.value = localStorage.getItem("inputDistortion");
 }
 
-if (localStorage.getItem("inputPingPong")) {
-    inputPingPong.value = localStorage.getItem("inputPingPong");
+if (localStorage.getItem("inputPingPongDelay")) {
+    inputPingPongDelay.value = localStorage.getItem("inputPingPongDelay");
+}
+
+if (localStorage.getItem("inputPingPongFeedback")) {
+    inputPingPongFeedback.value = localStorage.getItem("inputPingPongFeedback");
 }
 
 if (localStorage.getItem("inputReverb")) {
@@ -22,19 +45,62 @@ if (localStorage.getItem("inputReverb")) {
 inputDistortion.addEventListener("change", function() {
     localStorage.setItem("inputDistortion", inputDistortion.value);
 });
-inputPingPong.addEventListener("change", function() {
-    localStorage.setItem("inputPingPong", inputPingPong.value);
+inputPingPongDelay.addEventListener("change", function() {
+    localStorage.setItem("inputPingPongDelay", inputPingPongDelay.value);
+});
+inputPingPongFeedback.addEventListener("change", function() {
+    localStorage.setItem("inputPingPongFeedback", inputPingPongFeedback.value);
 });
 inputReverb.addEventListener("change", function() {
     localStorage.setItem("inputReverb", inputReverb.value);
 });
 
-function parameterChanges() {
-    var parameterChangeFlex = document.getElementById("parameterChanges");
-    if (parameterChangeFlex.style.display === "none") {
-        parameterChangeFlex.style.display = "block";
+function changeDistortion() {
+    var parameterChangeFlexDistortion = document.getElementById("inputDistortion");
+    if (parameterChangeFlexDistortion.style.display === "none") {
+        parameterChangeFlexDistortion.style.display = "block";
     } else {
-        parameterChangeFlex.style.display = "none";
+        parameterChangeFlexDistortion.style.display = "none";
+    }
+}
+
+function changePingPong() {
+    var parameterChangeFlexDelay = document.getElementById("inputPingPongDelay");
+    var parameterChangeFlexDelayLabel = document.getElementById("inputPingPongDelayLabel");
+    var parameterChangeFlexFeedback = document.getElementById("inputPingPongFeedback");
+    var parameterChangeFlexFeedbackLabel = document.getElementById("inputPingPongFeedbackLabel");
+
+    if (parameterChangeFlexDelay.style.display === "none") {
+        parameterChangeFlexDelay.style.display = "block";
+    } else {
+        parameterChangeFlexDelay.style.display = "none";
+    }
+
+    if (parameterChangeFlexFeedback.style.display === "none") {
+        parameterChangeFlexFeedback.style.display = "block";
+    } else {
+        parameterChangeFlexFeedback.style.display = "none";
+    }
+
+    if (parameterChangeFlexDelayLabel.style.display === "none") {
+        parameterChangeFlexDelayLabel.style.display = "block";
+    } else {
+        parameterChangeFlexDelayLabel.style.display = "none";
+    }
+
+    if (parameterChangeFlexFeedbackLabel.style.display === "none") {
+        parameterChangeFlexFeedbackLabel.style.display = "block";
+    } else {
+        parameterChangeFlexFeedbackLabel.style.display = "none";
+    }
+}
+
+function changeReverb() {
+    var parameterChangeFlexReverb = document.getElementById("inputReverb");
+    if (parameterChangeFlexReverb.style.display === "none") {
+        parameterChangeFlexReverb.style.display = "block";
+    } else {
+        parameterChangeFlexReverb.style.display = "none";
     }
 }
 
@@ -47,7 +113,7 @@ function synthSoundsStop() {
 
     Tone.Transport.stop()
     Tone.Transport.cancel()
-    Tone.PolySynth.disconnect()
+    // Tone.PolySynth.disconnect()
 
 }
 
@@ -65,29 +131,14 @@ function synthSoundsStop() {
 async function synthTone() {
 
     await Tone.start()
-    console.log('audio is ready')
-
-    console.log(inputDistortion)
 
     // create two monophonic synths
     const synthA = new Tone.FMSynth().toDestination();
     const synthB = new Tone.AMSynth().toDestination();
 
-    //create a distortion effect
-    console.log(inputDistortion.value)
-
-    const dist = new Tone.Distortion(inputDistortion.value).toDestination();
-
-    console.log(dist)
-
-
-    //create a reverb effect
-    const reverb = new Tone.Reverb(10).toDestination();
-
-    const pingPong = new Tone.PingPongDelay(inputPingPong.value + "n", 0.8).toDestination();
-
-    console.log(pingPong)
-
+    const distortion = new Tone.Distortion(inputDistortion.value).toDestination();
+    const pingPong = new Tone.PingPongDelay(inputPingPongDelay.value + "n", inputPingPongFeedback.value).toDestination();
+    const reverb = new Tone.Reverb(inputReverb.value).toDestination();
 
     // //play a note every quarter-note
     // const loopA = new Tone.Loop(time => {
@@ -105,7 +156,12 @@ async function synthTone() {
     const synth = new Tone.PolySynth().toDestination();
     // set the attributes across all the voices using 'set'
     // synth.set({ detune: -1200 });
-    synth.triggerAttackRelease(["C4", "E4", "A4"], .5).connect(pingPong).connect(reverb);
+    synth.triggerAttackRelease(["C4", "E4", "A4"], "2n").connect(pingPong).connect(reverb).connect(distortion);
+
+    synth.triggerAttackRelease(["C4", "E4", "A4"], "2n", now+0.5).connect(pingPong).connect(reverb).connect(distortion);
+
+
+
 
     // ramp up to 800 bpm over 10 seconds
     // Tone.Transport.bpm.rampTo(120, 10);
