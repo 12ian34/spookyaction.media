28dfaa0ac3a8f2b3b766bcb63ea6933a467fed51
Ian Ahuja <12ian34@gmail.com>
2021-02-17 19:21:14 +0000

    changed input to sliders, attempted live change for distortion

--- public/js/synth.js
+++ public/js/synth.js
@@ -2,6 +2,8 @@
 // console.log(inputDistortion)
 // console.log(inputDistortion.value)
 
+// const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require("constants");
+
 // const { addListener } = require("process");
 
 var inputDistortion = document.getElementById('inputDistortion');
@@ -12,23 +14,52 @@ var inputPingPongFeedback = document.getElementById('inputPingPongFeedback');
 var inputPingPongFeedbackLabel = document.getElementById('inputPingPongFeedbackLabel');
 var inputReverb = document.getElementById("inputReverb");
 var inputReverbLabel = document.getElementById("inputReverbLabel");
+var inputPitchShift = document.getElementById("inputPitchShift");
+var inputPitchShiftLabel = document.getElementById("inputPitchShiftLabel");
 
-var distortionElements = [
+var buttonSoundStart = document.getElementById('buttonSoundStart');
+var buttonSoundStop = document.getElementById('buttonSoundStop');
+var buttonDistortion = document.getElementById('buttonDistortion');
+var buttonPingPong = document.getElementById('buttonPingPong');
+var buttonReverb = document.getElementById('buttonReverb');
+var buttonPitchShift = document.getElementById('buttonPitchShift');
+
+var inputDistortionValue = document.getElementById("inputDistortionValue");
+var inputPingPongDelayValue = document.getElementById("inputPingPongDelayValue");
+var inputPingPongFeedbackValue = document.getElementById("inputPingPongFeedbackValue");
+var inputReverbValue = document.getElementById("inputReverbValue");
+var inputPitchShiftValue = document.getElementById("inputPitchShiftValue");
+
+inputDistortionValue.innerHTML = inputDistortion.value;
+inputPingPongDelayValue.innerHTML = inputPingPongDelay.value;
+inputPingPongFeedbackValue.innerHTML = inputPingPongFeedback.value;
+inputReverbValue.innerHTML = inputReverb.value;
+inputPitchShiftValue.innerHTML = inputPitchShift.value;
+
+var elementsDistortion = [
     inputDistortion,
     inputDistortionLabel,
-]
+    inputDistortionValue,
+];
 
-var pingPongElements = [
+var elementsPingPong = [
     inputPingPongDelay,
     inputPingPongDelayLabel,
+    inputPingPongDelayValue,
     inputPingPongFeedback,
     inputPingPongFeedbackLabel,
-]
+    inputPingPongFeedbackValue,
+];
 
-var reverbElements = [
+var elementsReverb = [
     inputReverb,
     inputReverbLabel,
-]
+];
+
+var elementsPitchShift = [
+    inputPitchShift,
+    inputPitchShiftLabel,
+];
 
 var storableParameters = [
     inputDistortion,
@@ -36,14 +67,147 @@ var storableParameters = [
     inputPingPongDelay,
     inputPingPongFeedback,
     inputReverb,
-]
+];
 
-document.getElementById('sounds').addEventListener('click', synthTone)
-document.getElementById('stop').addEventListener('click', synthSoundsStop)
+buttonSoundStart.addEventListener('click', synthTone);
+buttonSoundStop.addEventListener('click', synthSoundsStop);
 
-document.getElementById('showHideDistortion').addEventListener('click', showHideDistortion)
-document.getElementById('showHidePingPong').addEventListener('click', showHidePingPong)
-document.getElementById('showHideReverb').addEventListener('click', showHideReverb)
+buttonDistortion.addEventListener('click', toggleDistortion);
+buttonPingPong.addEventListener('click', togglePingPong);
+buttonReverb.addEventListener('click', toggleReverb);
+buttonPitchShift.addEventListener('click', togglePitchShift);
+
+inputDistortion.oninput = function() {
+    inputDistortionValue.innerHTML = this.value;
+}
+
+inputPingPongDelay.oninput = function() {
+    inputPingPongDelayValue.innerHTML = this.value;
+}
+
+inputPingPongFeedback.oninput = function() {
+    inputPingPongFeedbackValue.innerHTML = this.value;
+}
+
+inputReverb.oninput = function() {
+    inputReverbValue.innerHTML = this.value;
+}
+
+inputPitchShift.oninput = function() {
+    inputPitchShiftValue.innerHTML = this.value;
+}
+
+
+function getFromLocalStorage(parameter) {
+    if (localStorage.getItem(String(parameter.id))) {
+        parameter.value = localStorage.getItem(String(parameter.id));
+    };
+};
+
+function checkAndStoreParameterChanges(parameter) {
+    parameter.addEventListener("change", function() {
+        localStorage.setItem(String(parameter.id), parameter.value);
+        console.log("fuck2")
+
+    });
+};
+
+function hideElements(parameter) {
+    if (parameter.style.display === "none") {
+        parameter.style.display = "block";
+    } else {
+        parameter.style.display = "none"
+    };
+};
+
+function toggleDistortion() {
+    for (let i = 0; i < elementsDistortion.length; i++) {
+        hideElements(elementsDistortion[i]);
+    };
+};
+
+function togglePingPong() {
+    for (let i = 0; i < elementsPingPong.length; i++) {
+        hideElements(elementsPingPong[i]);
+    };
+};
+
+function toggleReverb() {
+    for (let i = 0; i < elementsReverb.length; i++) {
+        hideElements(elementsReverb[i]);
+    };
+};
+
+function togglePitchShift() {
+    for (let i = 0; i < elementsPitchShift.length; i++) {
+        hideElements(elementsPitchShift[i]);
+    };
+};
+
+function synthSoundsStop() {
+    Tone.Transport.stop()
+    Tone.Transport.cancel()
+        // Tone.PolySynth.disconnect()
+};
+
+async function synthTone() {
+
+    await Tone.start();
+    const now = Tone.now()
+
+    // create two monophonic synths
+    // const synthA = new Tone.FMSynth().toDestination();
+    // const synthB = new Tone.AMSynth().toDestination();
+
+    const synth = new Tone.PolySynth().toDestination();
+
+    if (inputPingPongDelay.value != 0 || inputPingPongFeedback.value != 0) {
+        var pingPong = new Tone.PingPongDelay(inputPingPongDelay.value + "n", inputPingPongFeedback.value).toDestination();
+        synth.connect(pingPong)
+    };
+
+    if (inputDistortion.value != 0) {
+        var distortion = new Tone.Distortion(inputDistortion.value).toDestination();
+        synth.connect(distortion)
+    };
+
+    if (inputReverb.value != 0) {
+        var reverb = new Tone.Reverb(inputReverb.value).toDestination();
+        synth.connect(reverb)
+    };
+
+    if (inputPitchShift.value != 0) {
+        var pitchShift = new Tone.PitchShift({
+            pitch: inputPitchShift.value,
+            delayTime: 2
+        }).toDestination();
+        synth.connect(pitchShift)
+    };
+
+    inputDistortion.addEventListener("change", function() {
+        distortion.distortion = inputDistortion.value;
+        // distortion.set('distortion', inputDistortion.value);
+    });
+
+    synth.triggerAttackRelease(["C4", "E4", "A4"], "2")
+
+    // connect(pingPong).connect(reverb).connect(distortion).connect(pitchShift);
+    // synth.triggerAttackRelease(["C4", "E4", "A4"], "2n", now + 0.5).connect(pingPong).connect(reverb).connect(distortion).connect(pitchShift);
+}
+
+for (let i = 0; i < storableParameters.length; i++) {
+    getFromLocalStorage(storableParameters[i]);
+};
+
+
+
+for (let i = 0; i < storableParameters.length; i++) {
+    checkAndStoreParameterChanges(storableParameters[i]);
+};
+
+
+
+// // old snippets
 
 // var inputDataSynth = {
 //     'inputDistortion': 0,
@@ -64,51 +228,6 @@ document.getElementById('showHideReverb').addEventListener('click', showHideReve
 //     inputReverb.value = JSON.parse(inputDataSynth("inputReverb");
 // }
 
-function getFromLocalStorage(parameter) {
-    if (localStorage.getItem(String(parameter.id))) {
-        parameter.value = localStorage.getItem(String(parameter.id));
-    }
-}
-
-function storeParameterChanges(parameter) {
-    parameter.addEventListener("change", function() {
-        localStorage.setItem(String(parameter.id), parameter.value);
-    });
-}
-
-function hideElements(parameter) {
-    if (parameter.style.display === "none") {
-        parameter.style.display = "block";
-    } else {
-        parameter.style.display = "none"
-    }
-}
-
-function showHideDistortion() {
-    for (let i = 0; i < distortionElements.length; i++) {
-        hideElements(distortionElements[i]);
-    }
-}
-
-function showHidePingPong() {
-    for (let i = 0; i < pingPongElements.length; i++) {
-        hideElements(pingPongElements[i]);
-    }
-}
-
-function showHideReverb() {
-    for (let i = 0; i < reverbElements.length; i++) {
-        hideElements(reverbElements[i]);
-    }
-}
-
-function synthSoundsStop() {
-
-    Tone.Transport.stop()
-    Tone.Transport.cancel()
-        // Tone.PolySynth.disconnect()
-
-}
 
 // window.onload = function() {
 //     if (sessionStorage.getItem("autosave")) {
@@ -117,47 +236,21 @@ function synthSoundsStop() {
 //     }
 // }
 
-async function synthTone() {
+// //play a note every quarter-note
+// const loopA = new Tone.Loop(time => {
+//     synthA.triggerAttackRelease("C2", "16n", time);
+// }, "4n").start(0);
 
-    await Tone.start()
+// //play another note every off quarter-note, by starting it "8n"
+// const loopB = new Tone.Loop(time => {
+//     synthB.triggerAttackRelease("C3", "16n", time);
+// }, "4n").start("4n");
 
-    // create two monophonic synths
-    const synthA = new Tone.FMSynth().toDestination();
-    const synthB = new Tone.AMSynth().toDestination();
+// // the loops start when the Transport is started
+// Tone.Transport.start()
 
-    const distortion = new Tone.Distortion(inputDistortion.value).toDestination();
-    const pingPong = new Tone.PingPongDelay(inputPingPongDelay.value + "n", inputPingPongFeedback.value).toDestination();
-    const reverb = new Tone.Reverb(inputReverb.value).toDestination();
+// set the attributes across all the voices using 'set'
+// synth.set({ detune: -1200 });
 
-    // //play a note every quarter-note
-    // const loopA = new Tone.Loop(time => {
-    //     synthA.triggerAttackRelease("C2", "16n", time).connect(dist).connect(reverb);
-    // }, "4n").start(0);
-
-    // //play another note every off quarter-note, by starting it "8n"
-    // const loopB = new Tone.Loop(time => {
-    //     synthB.triggerAttackRelease("C3", "16n", time).connect(dist).connect(reverb);
-    // }, "4n").start("8n");
-
-    // // the loops start when the Transport is started
-    // Tone.Transport.start()
-
-    const synth = new Tone.PolySynth().toDestination();
-    // set the attributes across all the voices using 'set'
-    // synth.set({ detune: -1200 });
-    synth.triggerAttackRelease(["C4", "E4", "A4"], "2n").connect(pingPong).connect(reverb).connect(distortion);
-
-    synth.triggerAttackRelease(["C4", "E4", "A4"], "2n", now + 0.5).connect(pingPong).connect(reverb).connect(distortion);
-
-    // ramp up to 800 bpm over 10 seconds
-    // Tone.Transport.bpm.rampTo(120, 10);
-
-}
-
-for (let i = 0; i < storableParameters.length; i++) {
-    getFromLocalStorage(storableParameters[i]);
-}
-
-for (let i = 0; i < storableParameters.length; i++) {
-    storeParameterChanges(storableParameters[i]);
-}
+// ramp up to 800 bpm over 10 seconds
+// Tone.Transport.bpm.rampTo(120, 10);
